394e160e1a9f0e227d8641030976f602714d1393
cvsyn <xlucyv@gmail.com>
2026-02-12 01:17:04 +0900

    Make issued_by_agent migration compatible with older Postgres

--- migrations/007_issued_by_agent.sql
+++ migrations/007_issued_by_agent.sql
@@ -1,4 +1,14 @@
-ALTER TABLE entities ADD COLUMN IF NOT EXISTS issued_by_agent_name TEXT;
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM information_schema.columns
+    WHERE table_name = 'entities'
+      AND column_name = 'issued_by_agent_name'
+  ) THEN
+    ALTER TABLE entities ADD COLUMN issued_by_agent_name TEXT;
+  END IF;
+END $$;
 
 DO $$
 BEGIN
@@ -13,4 +23,13 @@ BEGIN
   END IF;
 END $$;
 
-CREATE INDEX IF NOT EXISTS idx_entities_issued_by_agent_name ON entities(issued_by_agent_name);
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_indexes
+    WHERE indexname = 'idx_entities_issued_by_agent_name'
+  ) THEN
+    CREATE INDEX idx_entities_issued_by_agent_name ON entities(issued_by_agent_name);
+  END IF;
+END $$;
